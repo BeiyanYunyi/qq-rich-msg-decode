c3c98240719f5c3582e25860b3ffc6a4e1d44972
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-03-12 21:07:43 +0800

    refactor: :fire: Remove unnecessary code

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,12 @@
 mod utils;
 use protobuf::Message;
+use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
 include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(value: &str);
-}
-
 #[wasm_bindgen(js_name = parsePicRec)]
 pub fn parse_pic_rec(content: &[u8]) -> String {
+    set_panic_hook();
     let result = RichMsg::PicRec::parse_from_bytes(content).expect("Failed to parse");
     return protobuf_json_mapping::print_to_string(&result).expect("Failed to convert to JSON");
 }
